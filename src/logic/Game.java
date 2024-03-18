e08e3ba345ff904d00b802c595ee49bc95a19f03
koubek <koubek.jenda@icloud.com>
2024-03-18 09:46:10 +0200

    added methods

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,13 +1,27 @@
 package src.logic;
 
+import javax.swing.*;
+
 public class Game {
 
-    public static char switchPlayer(char player) {
+    public char switchPlayer(char player) {
         if (player == 'X') {
             return 'O';
         } else {
             return 'X';
         }
+    }
+
+    public boolean checkWin() {
+        return false;
+    }
+
+    private boolean checkDraw() {
+        return false;
+    }
+
+    private void resetGame() {
+
     }
 
 }
