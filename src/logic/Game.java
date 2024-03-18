24f8fc1eb30ae8f5a830d628694ddd7c2cd9419d
koubek <koubek.jenda@icloud.com>
2024-03-18 09:36:42 +0200

    Removed useless fields and methods

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -2,28 +2,6 @@ package src.logic;
 
 public class Game {
 
-    private static Game game;
-
-    private Tile[][] tiles;
-
-    public static void newGame() {
-        game = new Game();
-    }
-
-    private Game() {
-        tiles = new Tile[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                tiles[i][j] = new Tile();
-            }
-        }
-    }
-
-    public static void setTilePressed(int x, int y, int player) {
-        game.tiles[x][y].setUsed(true);
-        game.tiles[x][y].setPlayer(player);
-    }
-
     public static char switchPlayer(char player) {
         if (player == 'X') {
             return 'O';
